9fcb661d21a2f04d9f6769c1bfef9f6ab1f2bb7a
Naveen Kharwar <naveenkharwar@hotmail.com>
2020-07-23 21:27:16 +0530

    Add: CTA Section Skeleton

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   purge: {
     enabled: false,
@@ -11,5 +13,16 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase, config }) {
+      addBase({
+        'h1': { fontSize: config('theme.fontSize.4xl') },
+        'h2': { fontSize: config('theme.fontSize.3xl') },
+        'h3': { fontSize: config('theme.fontSize.2xl') },
+        'h4': { fontSize: config('theme.fontSize.xl') },
+        'h5': { fontSize: config('theme.fontSize.lg') },
+        'h6': { fontSize: config('theme.fontSize.base') },
+      })
+    })
+  ],
 }
