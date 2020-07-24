35b06dc47d3a3ebd66347867e5b35f5d073aea50
Naveen Kharwar <naveenkharwar@hotmail.com>
2020-07-24 18:52:31 +0530

    Style: About Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,17 @@ module.exports = {
   },
   theme: {
     extend: {
+      fontFamily: {
+        'heading': "'Mulish', sans-serif",
+        'body': "'IBM Plex Sans', sans-serif"
+      },
       container: {
         center: true,
       },
       colors: {
         'primary': '#f4a100',
-        'secondary': '#00ba94'
+        'secondary': '#00ba94',
+        'accent': '#ed4840'
       }
     },
   },
@@ -20,12 +25,31 @@ module.exports = {
   plugins: [
     plugin(function({ addBase, config }) {
       addBase({
-        'h1': { fontSize: config('theme.fontSize.4xl') },
-        'h2': { fontSize: config('theme.fontSize.3xl') },
-        'h3': { fontSize: config('theme.fontSize.2xl') },
-        'h4': { fontSize: config('theme.fontSize.xl') },
-        'h5': { fontSize: config('theme.fontSize.lg') },
-        'h6': { fontSize: config('theme.fontSize.base') },
+        'body': { fontFamily: config("theme.fontFamily.body") },
+        'h1': { 
+          fontSize: config('theme.fontSize.4xl'),
+          fontFamily: config("theme.fontFamily.heading")
+        },
+        'h2': { 
+          fontSize: config('theme.fontSize.3xl'),
+          fontFamily: config("theme.fontFamily.heading")
+        },
+        'h3': { 
+          fontSize: config('theme.fontSize.2xl'),
+          fontFamily: config("theme.fontFamily.heading")
+        },
+        'h4': { 
+          fontSize: config('theme.fontSize.xl'),
+          fontFamily: config("theme.fontFamily.heading")
+        },
+        'h5': { 
+          fontSize: config('theme.fontSize.lg'),
+          fontFamily: config("theme.fontFamily.heading")
+        },
+        'h6': { 
+          fontSize: config('theme.fontSize.base'),
+          fontFamily: config("theme.fontFamily.heading")
+         },
       })
     })
   ],
